092cba80dc2f8ed597e0e7ed0924ca3f0a813bf7
ahbattah <ahbattah@users.noreply.github.com>
2018-03-20 15:57:02 +0300

    update

---
+++ XMLData.R
@@ -0,0 +1,16 @@
+library(XML)
+library(tidyverse)
+library(plyr)
+
+
+capitalGovernorate_xml <- xmlParse("xml/CapitalGovernorate.xml", encoding = "UTF-8")
+as.data.frame(CpitaCapitalGovernorate_xml)
+
+df <- data.frame(
+  Title = xpathSApply(capitalGovernorate_xml, "//item//title", xmlValue),
+  'Service Name' = xpathSApply(capitalGovernorate_xml, "//item//ibmwcm:element[@name = 'Service Name']//ibmwcm:value", xmlValue),
+  check.names = FALSE
+)
+
+nodeset <- getNodeSet(capitalGovernorate_xml, "/rss/channel/item")
+var.names <- lapply(nodeset, names)
